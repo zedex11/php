083551ce98708ac048596b839c6dd43be17f6bb7
Siarhei Hryshchanka <zedex@tut.by>
2022-05-12 16:44:05 +0300

    made some changes in our code

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 </head>
 <body>
 <center>
-<font color="green"><h1>Hello from Denis Astahov!</h1></font>
+<font color="green"><h1>Hello from World!</h1></font>
 <img src="logo.png" width=400>
 
 <?php
 echo '<p style="color: gold">
-      This is AWS Elastic Beanstalk WebServer with PHP App!!!!!!!!!!!!!!</p>';
+      This is AWS ECS container with PHP App!!!!!!!!!!!!!!</p>';
 echo '<br>Server IP:'.$_SERVER['SERVER_ADDR'];
 ?>
 <br>
 Version 2.2
-<font color="red"><h1>Hello From Canada!</h1></font>      
+<font color="red"><h1>Hello From Minsk!</h1></font>      
 </center>
 </body>
 </html>
